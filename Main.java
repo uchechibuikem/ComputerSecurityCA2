8be979d0a85fb321930f5a7b2bf0589877c79feb
Chibuikem Mathew Uche <chibuikemu13@gmail.com>
2025-11-23 20:47:37 +0000

    making of the menu

--- Main.java
+++ Main.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
+
+        boolean running = true;
+
+        while (running) {
+            printMenu();
+
+            System.out.print("Enter option: ");
+            String choice = scanner.nextLine().trim();
+
+            switch (choice) {
+                case "1" -> encryptFile();
+                case "2" -> decryptFile();
+                case "3" -> {
+                    System.out.println("Exiting application. Goodbye!");
+                    running = false;
+                }
+                default -> System.out.println("Invalid option. Please try again.\n");
+            }
+        }
 
     }
 }
